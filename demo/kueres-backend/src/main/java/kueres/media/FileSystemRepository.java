0babfb9e73dcd4d1ccc6a61bb7e377f4e8c07c5e
Tim Engbrocks <uynhg@student.kit.edu>
2021-01-16 19:00:13 +0100

    various changes

--- demo/kueres-backend/src/main/java/kueres/media/FileSystemRepository.java
+++ demo/kueres-backend/src/main/java/kueres/media/FileSystemRepository.java
@@ -1,42 +1,13 @@
 package kueres.media;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
-import org.springframework.stereotype.Repository;
 
-import kueres.utility.Utility;
+public interface FileSystemRepository {
 
-@Repository
-public class FileSystemRepository {
-
-	@Value("${media.dir}")
-	private String MEDIA_DIR;
-	
-	public String save(long id, byte[] content) throws IOException {
-		
-		Utility.LOG.info(MEDIA_DIR);
-		Utility.LOG.info("id: {}, content: {}", id, content.length);
-		
-		Path newFile = Paths.get(MEDIA_DIR + new Date().getTime() + "-" + id);
-		Files.createDirectories(newFile.getParent());
-		Files.write(newFile, content);
-		return newFile.toAbsolutePath().toString();
-		
-	}
-	
-	public FileSystemResource findByLocation(String location) {
-	    try {
-	        return new FileSystemResource(Paths.get(location));
-	    } catch (Exception e) {
-	        throw new ResourceNotFoundException(location + " not found in filesystem");
-	    }
-	}
+	public String save(long id, byte[] content) throws IOException;
+	public FileSystemResource findByLocation(String location) throws ResourceNotFoundException;
 	
 }
