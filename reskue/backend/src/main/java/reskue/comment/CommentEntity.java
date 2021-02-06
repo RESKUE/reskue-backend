ecb7ab919da3f3b9c914b886cfa91e974b9a8940
Jan Straßburg <uthhw@student.kit.edu>
2021-02-06 04:13:02 +0100

    Added Entites

--- reskue/backend/src/main/java/reskue/comment/CommentEntity.java
+++ reskue/backend/src/main/java/reskue/comment/CommentEntity.java
@@ -1,8 +1,62 @@
 package reskue.comment;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
 import kueres.base.BaseEntity;
+import kueres.media.MediaEntity;
+import reskue.ReskueEntity;
+import reskue.user.UserEntity;
 
 public class CommentEntity extends BaseEntity<CommentEntity>{
+	
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, optional = true)
+	@JoinColumn(name = "comments")
+	private ReskueEntity reskueEntity;
+	public static final String RESKUE_ENTITY = "reskueEntity";
+	public ReskueEntity getReskueEntity() { return this.reskueEntity; }
+	public void setReskueEntity(ReskueEntity reskueEntity) { this.reskueEntity = reskueEntity; }
+	
+	@Column(name = "text", nullable = false)
+	private String text = "";
+	public static final String TEXT = "text";
+	public String getText() { return this.text; }
+	public void setText(String text) { this.text = text; }
+	
+	// needs JSON Converter etc to connect to media entity
+	@OneToMany(mappedBy = "reskueEntity", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@Column(name = "media", nullable = false)
+	private List<MediaEntity> media = new ArrayList<MediaEntity>();
+	public static final String MEDIA = "media";
+	public List<MediaEntity> getMedia() { return this.media; }
+	public void setMedia(List<MediaEntity> media) { this.media = media; }
+	
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, optional = true)
+	@Column(name = "author", nullable = false)
+	private UserEntity author;
+	public static final String AUTHOR = "author";
+	public UserEntity getAuthor() { return this.author; }
+	public void setAuthor(UserEntity author) { this.author = author; }
+	
+	@Column(name = "createdAt", nullable = false)
+	private Date createdAt;
+	public static final String CREATED_AT = "createdAt";
+	public Date getCreatedAt() { return this.createdAt; }
+	public void setCreatedAt(Date createdAt) { this.createdAt = createdAt; }
+	
+	@Column(name = "updatedAt", nullable = false)
+	private Date updatedAt;
+	public static final String UPDATED_AT = "updatedAt";
+	public Date getUpdatedAt() { return this.updatedAt; }
+	public void setUpdatedAt(Date updatedAt) { this.updatedAt = updatedAt; }
 
 	@Override
 	public void applyPatch(CommentEntity details) {
--- reskue/backend/src/main/java/reskue/culturalasset/CulturalAssetEntity.java
+++ reskue/backend/src/main/java/reskue/culturalasset/CulturalAssetEntity.java
@@ -1,9 +1,79 @@
 package reskue.culturalasset;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+
 import reskue.ReskueEntity;
+import reskue.notification.NotificationEntity;
+import reskue.task.TaskEntity;
 
 public class CulturalAssetEntity extends ReskueEntity<CulturalAssetEntity>{
-
+	
+	@Column(name = "address", nullable = false)
+	private String address = "";
+	public static final String ADDRESS = "address";
+	public String getAddress() { return this.address; }
+	public void setAddress(String address) { this.address = address; }
+	
+	@Column(name = "longitude", nullable = false)
+	private double longitude = 0;
+	public static final String LONGITUDE = "longitude";
+	public double getLongitude() { return this.longitude; }
+	public void setLongitude(double longitude) { this.longitude = longitude; }
+	
+	@Column(name = "latitude", nullable = false)
+	private double latitude = 0;
+	public static final String LATITUDE = "latitude";
+	public double getLatitude() { return this.latitude; }
+	public void setLatitude(double latitude) { this.latitude = latitude; }
+	
+	@OneToMany(mappedBy = "culturalAsset", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@Column(name = "tasks", nullable = false)
+	private List<TaskEntity> tasks = new ArrayList<TaskEntity>();
+	public static final String TASKS = "tasks";
+	public List<TaskEntity> getTasks() { return this.tasks; }
+	public void setTasks(List<TaskEntity> tasks) { this.tasks = tasks; }
+	
+	@Column(name = "label", nullable = false)
+	private String label = "";
+	public static final String LABEL = "label";
+	public String getLabel() { return this.label; }
+	public void setLabel(String label) { this.label = label; }
+	
+	@Column(name = "level", nullable = false)
+	private int level = 0;
+	public static final String LEVEL = "level";
+	public int getLevel() { return this.level; }
+	public void setLevel(int level) { this.level = level; }
+	
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@Column(name = "culturalAssetParent", nullable = false)
+	private CulturalAssetEntity culturalAssetParent;
+	public static final String CULTURAL_ASSET_PARENT = "culturalAssetParent";
+	public CulturalAssetEntity getCulturalAssetParent() { return this.culturalAssetParent; }
+	public void setCulturalAssetParent(CulturalAssetEntity culturalAssetParent) { this.culturalAssetParent = culturalAssetParent; }
+	
+	@OneToMany(mappedBy = "culturalAssetParent", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@Column(name = "culturalAssetChildren", nullable = false)
+	private List<CulturalAssetEntity> culturalAssetChildren = new ArrayList<CulturalAssetEntity>();
+	public static final String CULTURAL_ASSET_CHILDREN = "culturalAssetChildren";
+	public List<CulturalAssetEntity> getCulturalAssetChildren() { return this.culturalAssetChildren; }
+	public void setCulturalAssetChildren(List<CulturalAssetEntity> culturalAssetChildren) { this.culturalAssetChildren = culturalAssetChildren; }
+	
+	@OneToMany(mappedBy = "entity", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@Column(name = "notifications", nullable = false)
+	private List<NotificationEntity> notifications = new ArrayList<NotificationEntity>();
+	public static final String NOTIFICATIONS = "notifications";
+	public List<NotificationEntity> getNotifications() { return this.notifications; }
+	public void setNotifications(List<NotificationEntity> notifications) { this.notifications = notifications; }
+	
 	@Override
 	public void applyPatch(CulturalAssetEntity details) {
 		// TODO Auto-generated method stub
