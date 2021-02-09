7208dd8dfde43379f3b0a6157f0ae6e2299108da
Tim Engbrocks <uynhg@student.kit.edu>
2021-02-09 17:19:26 +0100

    Changed urls inside spring-wait.sh to work inside docker containers

--- services/spring/spring-wait.sh
+++ services/spring/spring-wait.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 
-# Wait for:
-# - keycloak: curl http://localhost:5436/auth/realms/reskue
-# - rabbitmq: curl http://localhost:15437
-# - frost: curl http://localhost:5438/FROST-Server/v1.0
-#
-# Restart services manually:
-#   sudo docker-compose -f docker-compose.dev.yml --env-file dev.env restart keycloak rabbitmq frost
-
 echo "Waiting for services"
 
-responseKeycloak=$(curl --head --write-out '%{http_code}' --silent --output /dev/null http://localhost:5436/auth/realms/reskue)
-responseRabbitMQ=$(curl --head --write-out '%{http_code}' --silent --output /dev/null http://localhost:15437)
-responseFrost=$(curl --head --write-out '%{http_code}' --silent --output /dev/null http://localhost:5438/FROST-Server/v1.0)
+responseKeycloak=$(curl --head --write-out '%{http_code}' --silent --output /dev/null http://keycloak:8080/auth/realms/reskue)
+responseRabbitMQ=$(curl --head --write-out '%{http_code}' --silent --output /dev/null http://rabbitmq:15672)
+responseFrost=$(curl --head --write-out '%{http_code}' --silent --output /dev/null http://frost:8080/FROST-Server/v1.0)
 
 while [ "$responseKeycloak" -ne 200 ] || [ "$responseRabbitMQ" -ne 200 ] || [ "$responseFrost" -ne 200 ]; do
 
@@ -23,9 +15,9 @@ while [ "$responseKeycloak" -ne 200 ] || [ "$responseRabbitMQ" -ne 200 ] || [ "$
 
   sleep 1s
 
-  responseKeycloak=$(curl --head --write-out '%{http_code}' --silent --output /dev/null http://localhost:5436/auth/realms/reskue)
-  responseRabbitMQ=$(curl --head --write-out '%{http_code}' --silent --output /dev/null http://localhost:15437)
-  responseFrost=$(curl --head --write-out '%{http_code}' --silent --output /dev/null http://localhost:5438/FROST-Server/v1.0)
+  responseKeycloak=$(curl --head --write-out '%{http_code}' --silent --output /dev/null http://keycloak:8080/auth/realms/reskue)
+  responseRabbitMQ=$(curl --head --write-out '%{http_code}' --silent --output /dev/null http://rabbitmq:15672)
+  responseFrost=$(curl --head --write-out '%{http_code}' --silent --output /dev/null http://frost:8080/FROST-Server/v1.0)
 
 done
 
