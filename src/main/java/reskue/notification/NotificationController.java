33865f8bc65b8ef7cd2f80f814a9e8cfd29f6c69
Tim Engbrocks <uynhg@student.kit.edu>
2021-02-25 18:01:10 +0100

    Auto set sender/author routes and auto set endangered

--- src/main/java/reskue/notification/NotificationController.java
+++ src/main/java/reskue/notification/NotificationController.java
@@ -1,9 +1,23 @@
 package reskue.notification;
 
+import javax.annotation.security.RolesAllowed;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.keycloak.representations.AccessToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kueres.base.BaseController;
+import kueres.utility.Utility;
+import reskue.user.UserEntity;
+import reskue.user.UserService;
 
 /**
  * 
@@ -26,5 +40,29 @@ public class NotificationController
 	 * The API route for NotificationEntities.
 	 */
 	public static final String ROUTE = "/notification";
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/setSender")
+	@RolesAllowed("administrator")
+	public ResponseEntity<NotificationEntity> create(
+			@Valid @RequestBody NotificationEntity entity,
+			HttpServletRequest request, 
+			HttpServletResponse response) {
+		
+		Utility.LOG.trace("NotificationController.create called.");
+		
+		KeycloakAuthenticationToken authToken = (KeycloakAuthenticationToken) request.getUserPrincipal();
+		AccessToken token = authToken.getAccount().getKeycloakSecurityContext().getToken();
+		
+		String subject = token.getSubject();
+		UserEntity sender = userService.me(subject);
+		entity.setSender(sender);
+		
+		NotificationEntity createdEntity = service.create(entity);
+		return ResponseEntity.ok().body(createdEntity);
+		
+	}
 
 }
--- src/main/java/reskue/notification/NotificationService.java
+++ src/main/java/reskue/notification/NotificationService.java
@@ -5,6 +5,9 @@ import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
 import kueres.base.BaseService;
+import kueres.event.EventType;
+import kueres.eventbus.EventConsumer;
+import kueres.utility.Utility;
 
 @Service
 public class NotificationService extends BaseService<NotificationEntity, NotificationRepository>{
@@ -16,4 +19,21 @@ public class NotificationService extends BaseService<NotificationEntity, Notific
 		this.routingKey = NotificationController.ROUTE;
 	}
 
+	@Override
+	public NotificationEntity create(NotificationEntity entity) {
+		
+		Utility.LOG.trace("NotificationService.create called.");
+		
+		if (entity.getType() == NotificationType.ALARM.type) {
+			entity.getEntity().setIsEndangered(1);
+		}
+		
+		NotificationEntity savedEntity = repository.save(entity);
+		
+		EventConsumer.sendEvent("NotificationService.create", EventType.CREATE.type, this.getIdentifier(), EventConsumer.writeObjectAsJSON(savedEntity));
+		
+		return savedEntity;
+		
+	}
+	
 }
