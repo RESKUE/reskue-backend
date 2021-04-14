3430c08790c46ed5969195532ca7f1fa350d9a6c
tim.engbrocks <uynhg@student.kit.edu>
2021-04-14 09:06:16 +0200

    Fixed image tag and made wait script more verbose

--- services/spring/spring-wait.sh
+++ services/spring/spring-wait.sh
@@ -13,30 +13,31 @@ responseFrost=$(curl --head --write-out '%{http_code}' --silent --output /dev/nu
 
 echo "Waiting for RabbitMQ to get ready at url $rabbitmq_url"
 while [ "$responseRabbitMQ" -ne 200 ]; do
-  echo "."
+  echo "... waiting for RabbitMQ at $rabbitmq_url"
+  echo "Response: $responseRabbitMQ"
   sleep 1s
   responseRabbitMQ=$(curl --head --write-out '%{http_code}' --silent --output /dev/null "$rabbitmq_url")
 done
+echo "RabbitMQ is up"
 
 echo "Waiting for FROST to get ready at $frost_url"
 while [ "$responseFrost" -ne 200 ]; do
-  echo "."
-  echo $responseFrost
+  echo "... waiting for FROST at $frost_url"
+  echo "Response: $responseFrost"
   sleep 1s
   responseFrost=$(curl --head --write-out '%{http_code}' --silent --output /dev/null "$frost_url")
 done
+echo "FROST is up"
 
 echo "Waiting for Keycloak to get ready at url $keycloak_url"
 while [ "$responseKeycloak" -ne 200 ]; do
-  echo "."
+  echo "... waiting for Keycloak at $keycloak_url"
+  echo "Response: $responseKeycloak"
   sleep 1s
   responseKeycloak=$(curl --head --write-out '%{http_code}' --silent --output /dev/null "$keycloak_url")
 done
+echo "Keycloak is up"
 
 echo "Services started"
-echo $responseKeycloak
-echo $responseRabbitMQ
-echo $responseFrost
-
 echo "Starting spring"
 exec "$@"
